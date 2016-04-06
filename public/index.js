e1b9185581e5c928dcbaba8dc538e3a8d0e2714d
mekoo <michael.jespersen@gmail.com>
2016-04-06 14:39:20 +0200

    no more bundle

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 var appSelector = 'mycph';
 var app = document.getElementById(appSelector);
 
-var L = require('L');
-
 var div = document.createElement("div");
 div.appendChild(document.createTextNode("mycph"));
 
 app.appendChild(div);
+
+console.log(L);
